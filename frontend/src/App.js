bc57cfd950d88322637abadd08cf463775e1fe0a
LeonardoBloom <leonardocolacogg@gmail.com>
2024-11-30 20:53:42 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/login/Login.jsx'
+
+function App() {
+  return (
+    <>
+      <Login
+      name = "Carlos"
+      />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/login/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Login = ({name}) => {
+
+  
+
+  return (
+    <>
+
+      <div> THis is the login page for {name}</div>
+      <div>
+        <form>
+
+        </form>
+      </div>
+    
+    </>
+  )
+}
+
+export default Login
